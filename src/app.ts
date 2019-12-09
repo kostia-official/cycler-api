6e0019aaad2cdfff0d63329ec6178bc236f1668b
kozzztya <kozzztya@gmail.com>
2019-12-09 12:06:46 +0200

    Disable cors

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,11 @@ const app: Application = express(feathers());
 app.configure(configuration());
 // Enable security, CORS, compression, favicon and body parsing
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: false
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
